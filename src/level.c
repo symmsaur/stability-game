840135a8cb13e314e123a71e4c5e2488e8f16a4c
Jacob Sandell <ytlander@hotmail.com>
2015-07-29 15:28:07 +0200

    Windows compatibility

--- src/level.c
+++ src/level.c
@@ -4,7 +4,8 @@
 #include "level.h"
 
 void load_level(const char* filename){
-  FILE* lvl_file = fopen(filename, "r");
+	FILE* lvl_file;
+	fopen_s(&lvl_file, filename, "r");
   for(int y = 0; y < BG_TILES_Y; y++){
     for (int x = 0; x < BG_TILES_X; x++){
         set_bg_tile(x, y, fgetc(lvl_file));
@@ -13,7 +14,8 @@ void load_level(const char* filename){
 }
 
 void save_level(const char* filename){
-  FILE* lvl_file = fopen(filename, "w");
+  FILE* lvl_file;
+  fopen_s(&lvl_file, filename, "w");
   for(int y = 0; y < BG_TILES_Y; y++){
     for (int x = 0; x < BG_TILES_X; x++){
       fputc(get_bg_tile(x,y), lvl_file);
